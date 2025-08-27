9d65306aa63309bfacf875ca61a835c897060b2d
kalashinikov47 <125178350+kalashinikov47@users.noreply.github.com>
2025-08-27 09:03:39 +0800

    Update fofa.sh

--- speedtest/fofa.sh
+++ speedtest/fofa.sh
@@ -80,6 +80,8 @@ case $city_choice in
         ;;
     6)
         city="Jiangsu"
+		stream="rtp/239.49.8.132:6000"
+		stream="rtp/239.49.1.10:6000"
         stream="udp/239.49.8.19:9614"
         channel_key="江苏"
         url_fofa=$(echo  '"udpxy" && country="CN" && region="Jiangsu" && protocol="http"' | base64 |tr -d '\n')
